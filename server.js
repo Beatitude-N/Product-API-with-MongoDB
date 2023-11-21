3028399ae0ccc2fdd11fbf390dc112cb442a9b8f
beatitude200 <beatitude102@gmail.com>
2023-11-21 05:08:28 +0100

    last commit

--- server.js
+++ server.js
@@ -16,9 +16,29 @@ app.use(express.json())
 
 
 // router
+
+
 app.get("/", (req, res) => {
     res.send("Hello NODE API")
 })
+
+
+
+
+app.post("/products", async(req, res)=>{
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product)
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({
+            message: error.message
+        })
+    }
+   console.log(req.body)
+})
+
+
 
 app.get("/products", async(req, res)=>{
     try {
@@ -31,9 +51,11 @@ app.get("/products", async(req, res)=>{
         })
     }
 })
-app.post("/products", async(req, res)=>{
+
+app.get("/products/:id", async(req, res)=>{
     try {
-        const product = await Product.create(req.body)
+        const {id} = req.params
+        const product = await Product.findById(id)
         res.status(200).json(product)
     } catch (error) {
         console.log(error.message);
@@ -41,10 +63,58 @@ app.post("/products", async(req, res)=>{
             message: error.message
         })
     }
-
-
-   console.log(req.body)
 })
+app.put("/products/:id", async(req, res)=>{
+    try {
+        const {id} = req.params
+        const product = await Product.findByIdAndUpdate(id, req.body)
+     
+        if (!product) {
+            return res.status(404).json({
+                 success: false, 
+                 message: "Product not found" 
+                });
+        }
+        const updateProduct = await Product.findById(id)
+        return res.status(200).json({
+            success: true, 
+            message: "Product updated successfully", 
+            data: updateProduct 
+           });
+
+        } catch (error) {
+        console.log(error.message);
+        res.status(500).json({
+            message: error.message
+        })
+    }
+})
+app.delete("/products/:id", async(req, res)=>{
+    try {
+        const {id} = req.params
+        const product = await Product.findByIdAndDelete(id, req.body)
+     
+        if (!product) {
+            return res.status(404).json({
+                 success: false, 
+                 message: "Product not found" 
+                });
+        }
+        const deletedProduct = await Product.findById(id)
+        return res.status(200).json({
+            success: true, 
+            message: `product with ID ${id} deleted successfully`, 
+            data: deletedProduct 
+           });
+
+        } catch (error) {
+        console.log(error.message);
+        res.status(500).json({
+            message: error.message
+        })
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`server runing on ${port}`)
